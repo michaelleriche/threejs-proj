ac9397e31b7e72f70c6989df1e77a94a9cb53590
Michael le Riche <michaelleriche@Michaels-Mac-mini.local>
2023-03-12 13:29:26 -0400

    moving cube working

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,28 @@
 import { useEffect } from 'react';
 import './App.css';
+import SceneInit from './components/SceneInit';
 import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import { Stats } from 'three/examples/jsm/libs/stats.module';
 
 function App() {
   useEffect(() => {
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z = 96;
-
-    const canvas = document.getElementById('myThreeJsCanvas');
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
     const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
     const boxMaterial = new THREE.MeshNormalMaterial();
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
 
-    const animate = () => {
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
-    };
-    animate();
+    const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+    const points = [];
+    points.push(new THREE.Vector3(-20, 0, 0));
+    points.push(new THREE.Vector3(0, 20, 0));
+    points.push(new THREE.Vector3(20, 0, 0));
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    test.scene.add(line);
+    test.scene.add(boxMesh);
   }, []);
 
   return (
